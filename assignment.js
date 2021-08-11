fcb141ba78a6a6e4a49a272ed033ca48ff5fae84
kumarharshit12 <88801031+kumarharshit12@users.noreply.github.com>
2021-08-12 01:52:38 +0530

    Add files via upload

---
+++ assignment.js
@@ -0,0 +1,20 @@
+{
+    "name": "current project",
+    "version":"1.0.0",
+    "description": "",
+    "main": "index.js",
+    "scripts": {
+        "harshit": "nodemon product.js
+        },
+        "author": "",
+        "license":"ISC",
+        "dependencies":{
+            "express":"^4.16.4",
+            "moongoose":"^5.5.7"
+        },    
+
+        "harshitdependencies": {
+            "dotenv": "^8.0.0",
+            "nodemon":"^1.19.0"
+}                          
+                           
